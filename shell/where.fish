da65226cc76e0a132fd6f7cb3bb57dcfcb33a86f
Revanth Reddy Nemtoor <developerrevanth123@gmail.com>
2026-08-03 01:25:18 +0530

    fix: correctly escape newlines in fish integration script

--- shell/where.fish
+++ shell/where.fish
@@ -1,8 +1,8 @@
 function where
     set -lx WHERE_SHELL "fish"
-    set -lx WHERE_ALIASES (alias | string join \n)
-    set -lx WHERE_FUNCTIONS (functions -n | string join \n)
-    set -lx WHERE_BUILTINS (builtin -n | string join \n)
-    set -lx WHERE_ABBRS (abbr --show | string join \n)
+    set -lx WHERE_ALIASES (alias | string join \\n)
+    set -lx WHERE_FUNCTIONS (functions -n | string join \\n)
+    set -lx WHERE_BUILTINS (builtin -n | string join \\n)
+    set -lx WHERE_ABBRS (abbr --show | string join \\n)
     command where $argv
 end
